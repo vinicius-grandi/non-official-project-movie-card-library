3a1a1750a56226a67ab4f6e9f2bf479f3c448f7f
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-02-10 13:26:38 -0300

    Updates

---
+++ precache-manifest.73137c13f1904707d5f4432ee464a346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a28daa8647d9f80ac16f3fe93f3f484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a1ad973d7f9259a5537",
+    "url": "/static/css/main.f1e46de2.chunk.css"
+  },
+  {
+    "revision": "d0289e35f4043c8938fd",
+    "url": "/static/js/2.11218735.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.11218735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1ad973d7f9259a5537",
+    "url": "/static/js/main.63d79987.chunk.js"
+  },
+  {
+    "revision": "84e5482291f212a94bb1",
+    "url": "/static/js/runtime-main.c7e298dc.js"
+  }
+]);
